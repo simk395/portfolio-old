31c01c89ee2e29006e81f76b9f0ea5275f38aff0
Simon Kong <simon.kong95@gmail.com>
2019-06-24 17:29:45 -0400

    carousel effects finished

--- src/test1.js
+++ src/test1.js
@@ -5,38 +5,34 @@ export class test1 extends Component {
   render() {
     return (
       <section id='skills'>
-        <h1>Experienced with:</h1>
-        <img className='skills-icon' src='./icons/html5.png' />
-        <img className='skills-icon' src='./icons/css.png' />
-        <img className='skills-icon' src='./icons/javascript.png' />
-        <h1>Some tools that I use:</h1>
+        {/* <h1 className='skills-text'>Experienced with:</h1> */}
+        <div id='skills-icon'>
+          <img className='skills-icon' src='./icons/html5.png' />
+          <img className='skills-icon' src='./icons/css.png' />
+          <img className='skills-icon' src='./icons/javascript.png' />
+        </div>
+        <h1 className='skills-text'>Some tools that I enjoy using:</h1>
         <div
-          class='main-carousel js-flickity'
+          className='main-carousel js-flickity'
           data-flickity-options='{ "cellAlign": "center", "contain": true, "wrapAround": true }'
         >
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/react.png' />
           </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/postgres.png' />
           </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/rails.png' />
           </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/node.png' />
           </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/sass.png' />
           </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
-          </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
-          </div>
-          <div class='carousel-cell'>
-            <img src='me.jpg' />
+          <div className='carousel-cell'>
+            <img className='carousel-icon' src='./icons/mongodb.jpg' />
           </div>
         </div>
       </section>
