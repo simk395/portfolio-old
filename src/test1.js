c27ffbe0862b7e953abea2ffd72b7d83d63a4a93
Simon Kong <simon.kong95@gmail.com>
2019-06-24 16:37:59 -0400

    skills icons

--- src/test1.js
+++ src/test1.js
@@ -5,7 +5,11 @@ export class test1 extends Component {
   render() {
     return (
       <section id='skills'>
-        <h1>Some tools I am experienced with</h1>
+        <h1>Experienced with:</h1>
+        <img className='skills-icon' src='./icons/html5.png' />
+        <img className='skills-icon' src='./icons/css.png' />
+        <img className='skills-icon' src='./icons/javascript.png' />
+        <h1>Some tools that I use:</h1>
         <div
           class='main-carousel js-flickity'
           data-flickity-options='{ "cellAlign": "center", "contain": true, "wrapAround": true }'
