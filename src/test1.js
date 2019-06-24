0da672cafe6f54462361a1b8dbb86c9c69b128cb
Simon Kong <simon.kong95@gmail.com>
2019-06-24 17:38:49 -0400

    removed shadow on carousel and added autoplay

--- src/test1.js
+++ src/test1.js
@@ -5,7 +5,7 @@ export class test1 extends Component {
   render() {
     return (
       <section id='skills'>
-        {/* <h1 className='skills-text'>Experienced with:</h1> */}
+        <h1 className='skills-text'>Experienced with:</h1>
         <div id='skills-icon'>
           <img className='skills-icon' src='./icons/html5.png' />
           <img className='skills-icon' src='./icons/css.png' />
@@ -14,7 +14,7 @@ export class test1 extends Component {
         <h1 className='skills-text'>Some tools that I enjoy using:</h1>
         <div
           className='main-carousel js-flickity'
-          data-flickity-options='{ "cellAlign": "center", "contain": true, "wrapAround": true }'
+          data-flickity-options='{ "cellAlign": "center", "contain": true, "wrapAround": true, "autoPlay":3000 }'
         >
           <div className='carousel-cell'>
             <img className='carousel-icon' src='./icons/react.png' />
