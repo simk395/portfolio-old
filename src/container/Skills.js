53cae94516d2980bec845daee37fe9f70068eff3
Simon Kong <simon.kong95@gmail.com>
2019-06-25 19:54:47 -0400

    doing onclick event for skills

--- src/container/Skills.js
+++ src/container/Skills.js
@@ -3,23 +3,43 @@ import Carousel from '../components/Carousel';
 import Icons from '../components/Skillicons';
 
 export class Skills extends Component {
+  shrinkColumn = e => {
+    console.log(e);
+    const html = document.querySelector('.html');
+    const htmlCol = document.querySelector('.html-column');
+    const css = document.querySelector('.css');
+    const cssCol = document.querySelector('.css-column');
+    const js = document.querySelector('.js-column');
+    switch (e.target.className) {
+      case 'html-column':
+        e.target.classList.add('shrink');
+        html.classList.add('skills-grow');
+        css.classList.remove('skills-grow');
+        cssCol.classList.remove('shrink');
+        break;
+      case 'css-column':
+        e.target.classList.add('shrink');
+        css.classList.add('skills-grow');
+        html.classList.remove('skills-grow');
+        htmlCol.classList.remove('shrink');
+        console.log('hello');
+        break;
+    }
+  };
   render() {
     return (
-      <section id='skills'>
+      <section onClick={this.shrinkColumn} id='skills'>
         <div className='html'>
-          <div className='html-column'>
-            <span>H</span>yper <span>T</span>ext<span>M</span>arkup
-            <span>L</span>anguage
-          </div>
-          <img className='skills-icon' src='./icons/html5.png' />
+          <div className='html-column'>HTML</div>
+          {/* <img className='skills-icon' src='./icons/html5.png' /> */}
         </div>
         <div className='css'>
           <div className='css-column'>CSS</div>
-          <img className='skills-icon' src='./icons/css.png' />
+          {/* <img className='skills-icon' src='./icons/css.png' /> */}
         </div>
         <div className='js'>
-          <div className='js-column'>column</div>
-          <img className='skills-icon' src='./icons/javascript.png' />
+          <div className='js-column'>JS</div>
+          {/* <img className='skills-icon' src='./icons/javascript.png' /> */}
         </div>
       </section>
     );
