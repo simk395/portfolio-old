392a2b5e9144c308c16adf6f28b79960ec71f162
Simon Kong <simon.kong95@gmail.com>
2019-06-29 21:00:46 -0400

    need to make slideshow only on home page

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import { Switch, Route } from 'react-router-dom';
 import Blogs from './container/Blogs';
 
 function App() {
-  let i = 0;
-  window.onload = disableScroll;
-  window.addEventListener('wheel', e => {
-    e.stopPropagation();
-    let ids = ['intro', 'bio', 'skills', 'tools', 'education'];
-    if (e.deltaY > 0 && i + 1 <= ids.length - 1) {
-      i += 1;
-      var elmnt = document.getElementById(ids[i]);
-      elmnt.scrollIntoView();
-    } else if (e.deltaY < 0 && i - 1 >= 0) {
-      i -= 1;
-      var elmnt = document.getElementById(ids[i]);
-      elmnt.scrollIntoView();
-    }
-    console.log(i);
-  });
-
-  function disableScroll() {
-    document.body.style.overflow = 'hidden';
-  }
-  function enableScroll() {
-    document.body.style.overflow = 'initial';
-  }
-
   return (
     <div className='main'>
       <Navbar />
--- src/container/Home.js
+++ src/container/Home.js
@@ -6,7 +6,33 @@ import Skills from './Skills';
 import Education from './Education';
 
 export class Home extends Component {
+  disableScroll = () => {
+    document.body.style.overflow = 'hidden';
+  };
+  enableScroll = () => {
+    document.body.style.overflow = 'initial';
+  };
   render() {
+    if (window.location.href === 'http://localhost:3000/') {
+      let i = 0;
+      window.onload = this.disableScroll;
+      window.addEventListener('wheel', e => {
+        e.stopPropagation();
+        let ids = ['intro', 'bio', 'skills', 'tools', 'education'];
+        if (e.deltaY > 0 && i + 1 <= ids.length - 1) {
+          i += 1;
+          var elmnt = document.getElementById(ids[i]);
+          elmnt.scrollIntoView();
+        } else if (e.deltaY < 0 && i - 1 >= 0) {
+          i -= 1;
+          var elmnt = document.getElementById(ids[i]);
+          elmnt.scrollIntoView();
+        }
+        console.log(i);
+      });
+    } else {
+      window.onload = this.enableScroll;
+    }
     return (
       <div>
         <Landing />
